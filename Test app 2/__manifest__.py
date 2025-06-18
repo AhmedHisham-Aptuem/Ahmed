c9e9de172ae1db064644dbfbb695722de289bcb7
AhmedHisham-Aptuem <ahmed.hisham@aptuem.com>
2025-06-18 16:51:16 +0300

    Add files via upload

---
+++ Test app 2/__manifest__.py
@@ -0,0 +1,55 @@
+#############################################################################
+#
+#    Aptuem solutions.
+#
+#    Copyright (C) 2025-TODAY Aptuem solutions(<https://aptuem.com>)
+#    Author: Ahmed Hisham (<https://aptuem.com>)
+#
+#    You can't modify this app under any circumstances.
+#
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{
+    "name": "Ahmed app",
+
+    'version': "18.0",
+
+    'category': "Sales, Purchases, Accounting, Warehouse, Manufacturing,'
+                'Project",
+
+    'summary': "This app adds a fild on Sales order. Keywords: App, add, field, sales, order,.",
+
+    'description': """
+    This is the description of the app that adds a field on the sales order.
+
+    Key Features:
+    - This is the key features of the app that adds a field on the sales order.
+
+    Benefits:
+    - This is the benefits of the app that adds a field on the sales order.
+""",
+
+
+    'author': 'Ahmed',
+
+    'website': "https://www.aptuem.com",
+
+    "depends": [''base', 'sale_management', 'purchase', 'account', 'hr',
+                'project', 'stock', 'stock_picking_batch', 'mrp''],
+
+    "data": [
+        "views/res_users_view.xml",
+        "security/security.xml",
+    ],
+
+    'images': ['static/description/banner.svg'],
+    'license': "OPL-1",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
