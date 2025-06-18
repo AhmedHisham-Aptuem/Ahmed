4a67c64c368dcae72d7452e4d30194665c79488e
AhmedHisham-Aptuem <ahmed.hisham@aptuem.com>
2025-06-18 16:51:48 +0300

    Delete Test app 2 directory

--- Test app 2/models/res_users.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    warehouse_ids = fields.Many2many(comodel_name='stock.warehouse', string='Warehouse')
